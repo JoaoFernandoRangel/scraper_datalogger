de3c7e6318311aa23e8ef258ad0bd91c2a3dcda9
JoaoFernandoRangel <joaofrguimaraes1@gmail.com>
2025-04-10 20:21:45 -0300

    Refactor scraper.py: Clean up comments and remove unnecessary emojis from print statements

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@ import os
 import json
 from bs4 import BeautifulSoup
 
-# URL da página
-url = "http://200.137.71.136/channels.php"  # Substitua pelo IP real ou URL desejado
+
+url = "http://200.137.71.136/channels.php"  
 
 # Garante que as pastas existam
 os.makedirs("paginas", exist_ok=True)
@@ -21,8 +21,7 @@ def extrair_tabela_para_json(html):
     for linha in linhas:
         colunas = linha.find_all("td")
         if len(colunas) != 6:
-            continue  # Ignora linhas que não têm 6 colunas
-
+            continue  
         item = {
             "nome": colunas[1].text.strip(),
             "valor": float(colunas[2].text.strip()),
@@ -38,25 +37,25 @@ try:
     while True:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-        # Salva o HTML
+        # Salva o conteudo do get
         html_filename = f"paginas/pagina_{timestamp}.html"
         response = requests.get(url)
         response.raise_for_status()
         with open(html_filename, "w", encoding="utf-8") as file:
             file.write(response.text)
-        print(f"✅ PHP salvo: {html_filename}")
+        print(f"PHP salvo: {html_filename}")
 
         # Extrai JSON
         json_data = extrair_tabela_para_json(response.text)
         json_filename = f"json_logs/dados_{timestamp}.json"
         with open(json_filename, "w", encoding="utf-8") as f:
             json.dump(json_data, f, indent=4, ensure_ascii=False)
-        print(f"✅ JSON salvo: {json_filename}")
+        print(f"JSON salvo: {json_filename}")
 
         time.sleep(0.3)
 
 except KeyboardInterrupt:
-    print("\n⏹️ Captura interrompida pelo usuário.")
+    print("\nCaptura interrompida pelo usuário.")
 
 except requests.exceptions.RequestException as e:
-    print(f"❌ Erro ao acessar a página: {e}")
+    print(f"Erro ao acessar a página: {e}")
